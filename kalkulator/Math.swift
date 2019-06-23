064b1657d0f3343b66e9953b40e78dc324159251
Akbar Putera <akbarputera65@gmail.com>
2019-06-24 06:16:34 +0700

    add Class Math

---
+++ kalkulator/Math.swift
@@ -0,0 +1,27 @@
+//
+//  Math.swift
+//  kalkulator
+//
+//  Created by Five Code Developmet on 24/06/19.
+//  Copyright © 2019 akbar. All rights reserved.
+//
+
+import Foundation
+
+class Math {
+    let number = [0,1,2,3,4,5,6,7,8]
+    let oprtr = ["/","x","*","+","-","="]
+    
+    var numberOfScreen: Double = 0.0
+    var previousNumber: Double = 0.0
+    var performingMath: Bool = false
+    var operation = 0
+    var hasil: Double = 0
+    
+    func hitung (_ number1: Double, operator: String, Number2: Double) -> Double {
+    
+        return hasil
+    }
+    
+    
+}
